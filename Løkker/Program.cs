5b82c6890a2b6bb97dc30f8e0f3722e82670d730
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-20 11:19:31 +0100

    Gått gjennom løkker og kommentert dem ut

--- Løkker/Program.cs
+++ Løkker/Program.cs
@@ -4,14 +4,34 @@
     {
         static void Main(string[] args)
 
-        {/*For-loop*/
-            string[] ord = new string[] { "det", "er", "gøy", };
-            for (int i = 0; i < ord.Length; i++)
-            {
-                Console.WriteLine(ord[i]);
-            }
-
-            /*foreach loop*/
+        //{/*For-loop*/
+        //    string[] ord = new string[] { "det", "er", "gøy", };
+        //    for (int i = 0; i < ord.Length; i++)
+        //    {
+        //        Console.WriteLine(ord[i]);
+        //    }
+        //    Console.WriteLine();
+        //    /*foreach loop*/
+        //    string[] strings = new string[] { "dream", "of", "an", "absolution" };
+        //    foreach (string s in strings)
+        //    {
+        //        Console.WriteLine(s);
+        //    }
+        //    Console.WriteLine();
+        //    /*While-løkker*/
+        //    bool condition = true;
+        //    int intr0 = 0;
+        //    while (condition)
+        //    {
+        //        if (intr0 < 69)
+        //        {
+        //            Console.WriteLine("Erwan er pen");
+        //            intr0++;
+        //        }
+        //        else { condition = false; }
+        //    }
+        //}
+        {
 
         }
     }
